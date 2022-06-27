35b43c9dff8b64b8f8593fedb580c9469250b283
olliesorensen <olliesorensen@gmail.com>
2022-06-27 11:33:56 +0200

    done with inference on jetson

--- inference_jetson.py
+++ inference_jetson.py
@@ -13,12 +13,55 @@ from model_EdgeSegNet import EdgeSegNet
 from model_GuideDepth import GuideDepth
 from model_DDRNet import DualResNet, BasicBlock
 from utils import *
+from torch2trt import torch2trt, tensorrt_converter, get_arg, trt, add_missing_trt_tensors, torch_dtype_to_trt
 
 
 """ Script for testing various networks at inference time. All timings are given in milliseconds """
 
 
-def inference_test(model_name, data_set):
+# REGISTER NEW CONVERTERS
+@tensorrt_converter('torch.nn.functional.silu')
+def convert_silu(ctx):
+    input = get_arg(ctx, 'input', pos=0, default=None)
+    output = ctx.method_return
+    input_trt = add_missing_trt_tensors(ctx.network, [input])[0]
+    
+    layer = ctx.network.add_activation(input_trt, trt.ActivationType.SIGMOID)
+    layer = ctx.network.add_elementwise(input_trt, layer.get_output(0), trt.ElementWiseOperation.PROD)
+    
+    output._trt = layer.get_output(0)
+    
+@tensorrt_converter('torch.nn.functional.linear')
+def convert_Linear(ctx):
+    input = ctx.method_args[0]
+    weight = get_arg(ctx, 'weight', 1, None)
+    bias = get_arg(ctx, 'bias', 2, None)
+    input_trt = add_missing_trt_tensors(ctx.network, [input])[0]
+    output = ctx.method_return
+
+    # reshape to ...xNx1x1
+    layer = ctx.network.add_shuffle(input_trt)
+    layer.reshape_dims = tuple(input_trt.shape) + (1, 1) 
+
+    bias_trt = trt.Weights(torch_dtype_to_trt(weight.dtype))
+    if bias is not None:
+        bias_trt = bias.detach().cpu().numpy()
+        
+    # add fully connected
+    layer = ctx.network.add_fully_connected(
+        input=layer.get_output(0),
+        num_outputs=int(weight.shape[0]),
+        kernel=weight.detach().cpu().numpy(),
+        bias=bias_trt)
+
+    # reshape back to N
+    layer = ctx.network.add_shuffle(layer.get_output(0))
+    layer.reshape_dims = tuple(output.shape[1:])
+
+    output._trt = layer.get_output(0)
+
+
+def inference_test(model_name, data_set, trt=True):
 
     # Define tasks based on dataset
     train_tasks = create_task_flags('all', data_set)
@@ -42,13 +85,25 @@ def inference_test(model_name, data_set):
     elif model_name == "DDRNet":
         model = DualResNet(BasicBlock, [2, 2, 2, 2], train_tasks, planes=32, spp_planes=128, head_planes=64).to(device)
     else:
-        raise ValueError   
+        raise ValueError 
 
+    if trt:
+        # create example data
+        if data_set == "nyuv2":
+            x = torch.ones((1, 3, 288, 384)).cuda()
+        else:
+            x = torch.ones((1, 3, 256, 512)).cuda()
+        model.eval()
+
+        # convert to TensorRT feeding sample data as input
+        model_trt = torch2trt(model, [x])
+        model = model_trt
+    
     model.eval()
 
     # Define start event, end event and number of repetitions
     starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
-    repetitions = 300
+    repetitions = 100
     timings = np.zeros((repetitions, 1))
 
     # GPU warm-up
@@ -68,15 +123,11 @@ def inference_test(model_name, data_set):
         for rep in range(repetitions):
             
             if data_set == "nyuv2":
-                #test_data = (255-0)*torch.rand((1, 3, 288, 384)) + 0
                 test_data = torch.randint(0, 256, (1, 3, 288, 384)).to(device)
                 test_data = test_data.to(torch.float32)
-                #test_data = test_data.to(device)
             else:
-                #test_data = (255-0)*torch.rand((1, 3, 256, 512)) + 0
                 test_data = torch.randint(0, 256, (1, 3, 256, 512)).to(device)
                 test_data = test_data.to(torch.float32)
-                #test_data = test_data.to(device)
 
             starter.record()
             _ = model(test_data)
@@ -102,12 +153,14 @@ if __name__ == "__main__":
     
     # Create timings for NYUV2 dataset
     for model in models:
-        model_mean, model_std = inference_test(model, "nyuv2")
+        model_mean, model_std = inference_test(model, "nyuv2", trt=False)
         nyuv2_timings[model] = [model_mean, model_std]
+    
+    print("Halfway there")
 
     # Create timings for CityScapes dataset
     for model in models:
-        model_mean, model_std = inference_test(model, "cityscapes")
+        model_mean, model_std = inference_test(model, "cityscapes", trt=False)
         cityscapes_timings[model] = [model_mean, model_std]
 
     print("Timings for all models on NYUV2 given in milliseconds")
